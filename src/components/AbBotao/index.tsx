d852a2a3e0bc2112fa9aca6f7022afcc8e333c05
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-06-09 16:43:15 -0300

    style: estilizando o componente AbBotao

--- src/components/AbBotao/index.tsx
+++ src/components/AbBotao/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+
+const BotaoEstilizado = styled.button`
+    background: #EB9B00;
+    padding: 16px 32px;
+    border: 2px solid #EB9B00;
+    color: #FFFFFF;
+    font-size: 20px;
+    cursor: pointer;
+    &:hover {
+        background: #B87900;
+        border: 2px solid #B87900;
+    }
+`
 
 export function AbBotao(){
     return (
-        <button>
+        <BotaoEstilizado>
             Clique Aqui
-        </button>
+        </BotaoEstilizado>
     );
 }
